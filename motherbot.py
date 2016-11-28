512f1323c1865c69b8ac8ce199c0a4008cab322b
oleng <tjioechandra.official@gmail.com>
2016-11-28 20:04:05 +0700

    Added valid() to validate if definitions contained in section/page. Fine tune create_keyword to adapt to data structure changes.
    Fixed some uncaught exceptions.
    Removed db_insert for good (until needed)

--- motherbot.py
+++ motherbot.py
@@ -20,7 +20,7 @@ from sqlalchemy.orm import sessionmaker
 from docdb import Library
 
 log = logging.getLogger(__name__)
-# load logging config in env variable
+# load logging config from env variable
 logging.config.dictConfig(ast.literal_eval(os.getenv('LOG_CFG')))
 
 engine = create_engine('sqlite:///docbot.db', echo=True)
@@ -29,15 +29,6 @@ session = Session()
 
 ''' LOCAL TEST CONFIGS '''
 path = os.path.expanduser('~/Google Drive/docs/python-2.7.12_docs/library/')
-# page = 'datetime.html'
-# fullpath = os.path.join(os.path.expanduser(path), page)
-
-def db_insert(table=None, version_id=None, version_major=None, 
-    version_minor=None, version_micro=None, topic=None, section=None, 
-    keyword=None, url=None, header=None, body=None, footer=None):
-    pass
-    # Create objects
-    # if table == 'Library':
 
 
 def build_definitions(fullpath):
@@ -95,9 +86,9 @@ def build_definitions(fullpath):
     '''
     db_table      = DOC_TOPIC.capitalize()  
     version_id    = ''.join(DOC_LONGVERSION.split('.'))
-    version_major = version_id[0]
-    version_minor = version_id[1]
-    version_micro = version_id[2:]
+    major = version_id[0]
+    minor = version_id[1]
+    micro = version_id[2:]
 
     def transform_relative_links(arg):
         """ Replaces internal anchor refs and relative urls with absolute path
@@ -139,45 +130,88 @@ def build_definitions(fullpath):
                     'h5': '##### ', 'h6': '###### '}
         return header[arg]
 
-    def create_keyword(section):
-        ''' [ Keywords ] 
-        - Evaluate if section contains valid definition & extract the strings
-        '''
-        dt_parent = section.dt.parent
-        keyword = ''
-        while 'id' in section.dt.attrs:
-            keyword = section.dt.attrs['id']
-            # log.debug('>>> id key: {0}'.format(str(keyword)))
-            return keyword
+    def valid(section):
+        """[ Keywords ] Evaluate if section contains valid definition"""
+        # log.debug(section)
         try:
-            if section.dt.code.next_sibling is not None:
-                # log.debug('Looking in code css class')
-                first = section.dt.code.text
-                second = section.dt.code.next_sibling.string
-                keyword = ''.join('{}{}'.format(first, second))
-                return keyword
+            if 'id' in section.dt.attrs:
+                return True
+            elif section.dt.code.next_sibling is not None:
+                return True
+            else:
+                log.warn('Skipping: can\'t find keyword in %s', section.dt.code)
+                return False
         except AttributeError as error:
-            log.error('[ Keywords error]: {}'.format(error))
-            log.warn("Skipping: can't find keyword in {}".format(section.dt.code))
+            log.error(error)
+            return False
+
+    def create_keywords(section):
+        """ [ Keywords ] Extract definition type (class, method, etc) and 
+        keyword strings"""
+        # log.debug('Creating keyword')
+        try:
+            if 'id' in section.dt.attrs:
+                keytype = section['class'][0]
+                _keyword = section.dt['id']
+                log.debug('[id]: (%s) %s', keytype, _keyword)
+            # elif section.dt.code.next_sibling is not None or \
+            elif section.dt.find(class_='descname'):
+                # for readability
+                keytype = section['class'][0]
+                if section.dt.find(class_='descclassname') is None: 
+                    log.debug('`Descclassname` not found')
+                    _keyword = section.dt.find(class_='descname').string
+                    log.debug('[css] _keyword: %s', _keyword)
+                elif section.dt.find(class_='descclassname') is not None:
+                    log.debug('Found css `descclassname`')
+                    descclass = section.dt.find(class_='descclassname').string
+                    log.debug('Found css `descname`')
+                    descname = section.dt.find(class_='descname').string
+                    _keyword = '{}{}'.format(descclass, descname)
+                    log.debug('[css] _keyword: %s', _keyword)
+
+            # separate the types based on the length of what we have so far
+            # default when there's only a single syntax specified in doc
+            if len(_keyword.split('.')) < 2:
+                log.debug('One word syntax %s', _keyword)
+                keyclass = keytype
+                return keytype, keyclass, _keyword
+            # everything else. 7 is too much, but there's a bug i think in this 
+            # doc url that cause an exception error only 6: docs/3/library/
+            # urllib.parse.html#urllib.parse.urllib.parse.SplitResult.geturl
+            elif 7 > len(_keyword.split('.')) > 1:
+                keyclass = _keyword.split('.')[0]
+                keyword = _keyword
+                for index, val in enumerate(_keyword.split('.')[1:]): 
+                    keyword += ', {}'.format(
+                                    '.'.join(_keyword.split('.')[index + 1:])
+                                    )
+            log.debug('%s', keyword)
+            return keytype, keyclass, keyword
+
+        except AttributeError as error:
+            log.error('Keywords error: %s', (error))
+            log.warn("Skipping: can't find keyword in %s", section.dt.code)
             return False
 
     def create_header(section):
-        ''' [ Header ] sections 
-        - Feed (if exists) relative URLs to transform_relative_links()
-        - Format html to markdown,
-        '''
+        """ [ Header ] Convert anchors/relative URLs to absolute paths if exists
+        then convert html markups to markdown."""
+        # log.debug('Creating header')
         if section.a is not None:
             internal_link = transform_relative_links(section.a['href'])
             section.a['href'] = internal_link
             url = section.a['href']
         else:
+            log.warn('No internal anchors/links found %s', section.string)
             url = None
         # < Hack > copy untouched section.dd 1st since we're destroying all 
         # spans & still need to manipulate them in body (section.dd) later
         store_dd = cp.copy(section.dd)
         for span in section.find_all('span'):
             span.unwrap()
-        section.dd = store_dd   # put copy back
+        # put copy back
+        section.dd = store_dd   
         # < Hack > fix annoying trailing space in <em>class </em> to avoid 
         # incorrect markdown formatting
         for em in section.dt.find_all('em', {'class': 'property'}):
@@ -196,72 +230,85 @@ def build_definitions(fullpath):
         # remove double markdown link tag
         newtmp = tmp_header.replace(".``", ".")
         # string is immutable
-        header = '{0}{1}'.format(newtmp.replace('[¶', ''), '-----\n')
+        header = '{0}{1}'.format(newtmp.replace('[¶', ''), '-----')
+        # log.info('Constructed %s', header)
         return header, url
 
     def create_body(section):
-        ''' [ Body ] section 
-        - Format the definition in Header
-        - Convert internal & relative url links to absolute paths
-        '''
+        """ [ Body ] Convert anchors/relative URLs to absolute paths if exists
+        Format the definition in Header"""
+        # log.debug('Creating body')
         for link in section.dd.select('a[href]'):
             if link is not None:
                 transform_link = transform_relative_links(link.attrs['href'])
                 link.attrs['href'] = transform_link
+            else:
+                log.debug('Link? %s', link)
         transform_body =[]
         for content in section.dd.contents:
             transform_body.append(str(content))
         body = str(h.handle(''.join(transform_body))).strip()
+        # log.debug('Constructed body: %s', body)
         return body
 
     def create_footer(section):
-        ''' [ Footer ]
-        - Include infos in docbots replies
-        '''
+        """ [ Footer ] Include infos in docbots replies"""
         # readme_link = link
         # sub_docbot = sub
         # '\n\n`>>>` [README]({0}) |  `>>>` [/r/DocBot/]({1}'.format(
             # readme_link, sub_docbot)
+        # log.debug('Creating footer')
         footer = None
         return footer
 
     ''' Extract data from page '''
     for section in _sections:
-        if create_keyword(section):
-            keyword = create_keyword(section)
+        if 'docutils' in section['class']:
+            log.warn('Skipping, not a syntax definition. %s', section)
+            continue
+        elif valid(section):
+            keytype, keyclass, keyword = create_keywords(section)
             header, url = create_header(section)
             body = create_body(section)
             footer = create_footer(section)
-
+            log.info('(%s) keytype: %s, keyclass: %s, keyword: %s', 
+                        version_id, keytype, keyclass, keyword)
+            log.info('`%s` section done.\n', 
+                        section.dt.find(class_='descname').string)
             ''' Store all the data
             Arguments:
                 table:          Database table name
                 version_id:     Int, main variable for user <version> query
-                version_major:  Int(major).x.x, i.e. 3.5.2, 2.7.12
-                version_minor:  x.int(minor).x
-                version_micro:  x.x.int(micro)
+                major:          Int(major).x.x, i.e. 3.5.2, 2.7.12
+                minor:          x.int(minor).x
+                micro:          x.x.int(micro)
                 topic:          Library (Python Doc **Section**)
                 section:        Python doc page name, based on module name
+                keytype:        Class, method, function or attribute
                 keyword:        Main variable for user <syntax> query
                 url:            Permalink to Python doc syntax definition 
                 header:         Syntax & argument part
                 body:           Definition part
                 footer:         Related document URLs & docbot information links
-            '''
-            doc = Library( 
-                    version_id, version_major, version_minor, version_micro, 
-                    DOC_TOPIC, DOC_SECTION, keyword, url, header, body, footer)
-            log.info('Adding to database: {}'.format(doc))
-            # is this safe?
+            # 
+            # '''
+            doc = Library(
+                    version_id=version_id, major=major, 
+                    minor=minor, micro=micro, 
+                    topic=DOC_TOPIC, module=DOC_SECTION, keytype=keytype, 
+                    keyclass=keyclass, keyword=keyword, 
+                    url=url, header=header, body=body, footer=footer,)
+            # commit only when all definitions added to session
             session.add(doc)
             session.flush()
     # commit the record the database
     session.commit()
 
-
 for root, dirs, filenames in os.walk(path):
     for fname in filenames:
         fullpath = os.path.join(root, fname)
-        log.info('Start importing definitions', exc_info=True)
+        log.info('Start importing definitions %s', fullpath , exc_info=True)
         build_definitions(fullpath)
+
+# build_definitions(path)
 
